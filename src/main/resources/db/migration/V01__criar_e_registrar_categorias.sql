d5b6ea976f69bccb9d33ca6ddd83563847d8e077
Elizeu Freitas <elizeu.freitas@outlook.com>
2019-02-13 22:10:49 -0200

    5.1 - Criando a migração e entidade de lançamento

--- src/main/resources/db/migration/V01__criar_e_registrar_categorias.sql
+++ src/main/resources/db/migration/V01__criar_e_registrar_categorias.sql
@@ -3,8 +3,8 @@ CREATE TABLE categoria (
 	nome VARCHAR(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-INSERT INTO categoria (nome) VALUES ('Lazer');
-INSERT INTO categoria (nome) VALUES ('Alimentação');
-INSERT INTO categoria (nome) VALUES ('Supermercado');
-INSERT INTO categoria (nome) VALUES ('Farmácia');
-INSERT INTO categoria (nome) VALUES ('Outros');
+INSERT INTO categoria (nome) values ('Lazer');
+INSERT INTO categoria (nome) values ('Alimentação');
+INSERT INTO categoria (nome) values ('Supermercado');
+INSERT INTO categoria (nome) values ('Farmácia');
+INSERT INTO categoria (nome) values ('Outros');
